391599c31437b8087744e025eabae6c5d5cd8f44
Mrdivyendalwadi <114602971+Divyen-Dalwadi2000@users.noreply.github.com>
2023-04-09 10:27:50 +0530

    Update One2.java

--- prac2/One2.java
+++ prac2/One2.java
@@ -3,7 +3,7 @@
 Write application that creates an array of double, to provide following functionality.
  display the length of the array and its elements.
  Display an array. (Use for each version of loop for display).
- compute the sume of the squares of these numbers.
+ compute the sum of the squares of these numbers.
  Determine Mean and Median of an array.
  Sort an array – Ascending and Descending. Use any two sorting algorithm. User can also select the sorting method.
  Search an element from the array, i.e. returns the location of the element of an array that matches an indicated value.
@@ -12,26 +12,27 @@ Write application that creates an array of double, to provide following function
 */
 package pack2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class One2 {
 
   public static void main(String[] args) {
-    int ch = 0,i,j;
+    int ch = 0, i, j;
     Scanner choice = new Scanner(System.in);
     Scanner arey = new Scanner(System.in);
-    double array[] = new double[];
 
-    System.out.print("Enter the Element of the array : ");
+    ArrayList<Double> list = new ArrayList<Double>();
 
-    for (i = 0; i < array.length; i++) {
-      array[i] = arey.nextDouble();
+    System.out.println(
+      "Enter the Element of the array (Press 'done' to finish): "
+    );
+
+    while (arey.hasNextDouble()) {
+      double num = arey.nextDouble();
+      list.add(num);
     }
-
-    for (i = 0; i < array.length; i++) {
-      System.out.print(array[i] + "  ");
-    }
-
 
     while (ch < 9) {
       System.out.println(
@@ -39,7 +40,7 @@ public class One2 {
       );
       System.out.println("Enter 2 for Display an array.  ");
       System.out.println(
-        "Enter 3 for compute the sume of the squares of these numbers."
+        "Enter 3 for compute the sum of the squares of these numbers."
       );
       System.out.println("Enter 4 for Determine Mean and Median of an array.");
       System.out.println(
@@ -54,32 +55,73 @@ public class One2 {
       System.out.println(ch);
       switch (ch) {
         case 1:
+          System.out.println(
+            "The length of the array is : " + list.size() + "\n"
+          );
+
+          for (int k = 0; k < list.size(); k++) {
+            System.out.print(list.get(k) + "  ");
+          }
           break;
         case 2:
+          for (double num : list) {
+            System.out.print(num + "  ");
+          }
           break;
         case 3:
+          double sqr = 0;
+
+          for (double num : list) {
+            sqr += num * num;
+          }
+          System.out.println("\nThe sum of squares :  " + sqr);
           break;
         case 4:
+          System.out.println("=============MEAN===============");
+          double mean = 0, median = 0, sum = 0;
+          for (double num : list) {
+            sum += num;
+          }
+          mean = sum / list.size();
+          System.out.println("\n The mean of the array is : " + mean);
+
+          // System.out.println("\n=============MEDIAN===============");
+          // int size = list.size();
+          // ArrayList<Double> cp1 = Arrays.copyOf(list , );
+          
+          // Arrays.sort(list);
+          // double mid1 = list.get(size / 2 - 1);
+          // double mid2 = list.get(size / 2);
+
+          // if (list.size() % 2 == 0) {
+          //   median = (mid1 + mid2) / 2;
+          // }
           break;
         case 5:
           int ch5 = 0;
-          // double temp, array[];
           while (ch5 < 3) {
-            System.out.println("\nEnter 1 for Bubble sort ");
+            System.out.println("\n\nEnter 1 for Bubble sort ");
             System.out.println("Enter 2 for Insertion sort ");
             System.out.println("Enter 3 for Exit ");
-
-            switch (ch) {
+            System.out.print("Enter Your choice5 : ");
+            ch5 = choice.nextInt();
+            switch (ch5) {
               case 1:
-                // for (i = 0; i < array.length(); i++) {
-                //   for (j = 0; j < array.length(); j++) {
-                //     if (array[j] > array[j + 1]) {
-                //       temp = arrray[i];
-                //       arrray[i] = arrray[i + 1];
-                //       array[i + 1] = temp;
-                //     }
-                //   }
-                // }
+                System.out.println(
+                  "\n===============Bubble Short=============== "
+                );
+                for (i = 0; i < list.size() - 1; i++) {
+                  for (j = 0; j < list.size() - 1; j++) {
+                    if (list.get(j) > list.get(j + 1)) {
+                      double temp = list.get(j);
+                      list.set(j, list.get(j + 1));
+                      list.set(j + 1, temp);
+                    }
+                  }
+                }
+                for (i = 0; i < list.size(); i++) {
+                  System.out.print(list.get(i) + " ");
+                }
                 break;
               case 2:
                 break;
