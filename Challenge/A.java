f5ea6de372c732af70454b754b1a27524cbd2499
Mrdivyendalwadi <114602971+Divyen-Dalwadi2000@users.noreply.github.com>
2023-04-24 00:49:20 +0530

    Create A.java

---
+++ Challenge/A.java
@@ -0,0 +1,15 @@
+package quiz;
+
+public class A {
+
+  public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 20;
+    String str1 = "Divyen";
+    String str2 = "Dalwadi";
+    System.out.println(num1 + num2 + str1 + str2);
+  }
+}
+
+
+// 30DivyenDalwadi
